271305b9fad49b327051ebeb49752a77691add78
Anish giri <anishgiri6393@gmail.com>
2025-10-22 10:48:25 +0530

    Update and rename store.jsx to store.js

--- fronted/src/redux/store.js
+++ fronted/src/redux/store.js
@@ -1,6 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
-import userSliceReducer from "./userSlice.jsx";
-import productSlideReducer from "./productSlide.jsx";
+import userSliceReducer from "./userSlice.js";
+import productSlideReducer from "./productSlide.js";
 
 export const store = configureStore({
   reducer: {
