1a9c1db7bdc5efae2a336dee7cbee933abf35352
Anish giri <anishgiri6393@gmail.com>
2025-10-22 11:14:40 +0530

    Update store.js

--- fronted/src/redux/store.js
+++ fronted/src/redux/store.js
@@ -1,5 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
-import userSliceReducer from "./userSlice.js";
+import userSliceReducer from "./userslice.js";
 import productSlideReducer from "./productSlide.js";
 
 export const store = configureStore({
